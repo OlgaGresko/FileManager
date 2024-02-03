11997b196523c1e5c6e2375020d56c2d05ee76c2
OlgaGresko <122181692+OlgaGresko@users.noreply.github.com>
2024-02-03 04:00:13 +0200

    feat: add ls command

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,31 @@
+import fs from "fs/promises";
+import { join } from "path";
+
+import throwErrorMessage from "../utils/throwErrorMassage.js";
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = process.cwd();
+  const files = await fs.readdir(dirPath);
+
+  const checkedFiles = await Promise.all(
+    files.map(async (file) => {
+      const filePath = join(dirPath, file);
+      const stats = await fs.stat(filePath);
+      const type = stats.isDirectory() ? "directory" : "file";
+      return { name: file, type };
+    })
+  );
+
+  const sortedFiles = checkedFiles.sort((a, b) => {
+    if (a.type === "directory" && b.type !== "directory") {
+      return -1;
+    } else if (a.type !== "file" && b.type === "file") {
+      return 1;
+    }
+    return a.name.localeCompare(b.name);
+  });
+
+  console.table(sortedFiles);
 };
 
-await list();
+export default list;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import readline from "readline/promises";
-import { fileURLToPath } from 'url';
-import { dirname, join, resolve } from 'path';
+// import { fileURLToPath } from "url";
+import { dirname, join, resolve } from "path";
 
 import parseEnv from "./cli/env.js";
+import list from "./fs/list.js";
 import capitalizeFirstLetter from "./utils/capitalizeFirstLetter.js";
 import showCurrentWorkingDirectory from "./utils/printCurrentWorkingDirectory.js";
 
 // const pathToFile = fileURLToPath(import.meta.url);
 // const dirName = dirname(pathToFile);
-
+// console.log("dirName", dirName);
 
 const name = capitalizeFirstLetter(parseEnv());
 console.log(`Welcome to the File Manager, ${name}`);
@@ -24,28 +25,31 @@ if (homeDir) {
   showCurrentWorkingDirectory();
 }
 
-rl.on("line", (line) => {
+rl.on("line", async line => {
   try {
     if (line.trim() === "up") {
-        const currentDir = process.cwd();
-        const parentDirectory = join(currentDir, '..');
-        if (resolve(currentDir) === resolve(parentDirectory)) {
-            showCurrentWorkingDirectory();
-            return;
-        } else {
-           process.chdir(parentDirectory); 
-        }
-        
+      const currentDir = process.cwd();
+      const parentDirectory = join(currentDir, "..");
+      if (resolve(currentDir) === resolve(parentDirectory)) {
+        showCurrentWorkingDirectory();
+        return;
+      } else {
+        process.chdir(parentDirectory);
+      }
     } else if (line.trim().startsWith("cd")) {
-      const [command, arg] = line.trim().split(' ');
-  
-      if (arg.toLowerCase().startsWith("c:/")) {
-        process.chdir(arg);
+      const [_, arg] = line.trim().split(" ");
+
+      if (arg.includes("/") || arg.includes("\\")) {
+        const partsOfPath = arg.split(/[\/\\]/);
+        const newPath = join(...partsOfPath);
+        process.chdir(newPath);
       } else {
         const newPath = join(process.cwd(), arg);
         process.chdir(newPath);
       }
-    }    else if (line.trim() === ".exit") {
+    } else if (line.trim() === "ls") {
+      await list();
+    } else if (line.trim() === ".exit") {
       rl.close();
     } else {
       console.log("Invalid input");
